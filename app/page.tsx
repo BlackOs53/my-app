f51ecea0cbe0eac7075616e551bfdf97240f0990
WhiteOs <brpyrz@gmail.com>
2026-03-08 15:56:55 +0300

    bjkbkj

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <hl>Bora Poyraz</hl>
+     <h1>Bora Poyraz</h1>
     </div>
   );
 }
